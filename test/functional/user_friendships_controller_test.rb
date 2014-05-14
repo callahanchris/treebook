2d461cf72df025443ebaaf6ee6bd46d6eaaae3f7
Chris Callahan <callahan.c@gmail.com>
2014-05-14 15:18:27 +0800

    Add decorators and js-routes gems to add AJAX for friend requests

--- test/functional/user_friendships_controller_test.rb
+++ test/functional/user_friendships_controller_test.rb
@@ -219,7 +219,7 @@ class UserFriendshipsControllerTest < ActionController::TestCase
 			setup do
 				@user_friendship = create(:pending_user_friendship, user: users(:chris))
 				sign_in users(:chris)
-				get :edit, id: @user_friendship
+				get :edit, id: @user_friendship.friend.profile_name
 			end
 
 			should "get edit and return success" do
